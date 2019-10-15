0dbd6a2707b3cfb3dff0c6ce76e33964a74ad205
Grenier Célestin <cgrenier003@ensc.fr>
2019-10-15 10:39:08 +0200

    Solver.Game class for App

---
+++ Taquin/Solver/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solver
+{
+    public class Game
+    {
+
+        private int[,] grid;
+        private int size;
+        private int gapCount;
+
+        public Game(int size, int gapCount)
+        {
+            this.grid = new int[size, size];
+            this.size = size;
+            this.gapCount = gapCount;
+
+            int k = 1;
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    this.grid[i, j] = k <= size * size - gapCount ? k++ : 0;
+        }
+
+        public int this[int i, int j]
+        {
+            get { return this.grid[i, j]; }
+        }
+
+        public bool MakeMove(int i1, int j1, int i2, int j2)
+        {
+            if (this.grid[i2, j2] != 0 || this.grid[i1, j1] == 0)
+                return false;
+
+            this.grid[i2, j2] = this.grid[i1, j1];
+            this.grid[i1, j1] = 0;
+            return true;
+        }
+
+    }
+}
