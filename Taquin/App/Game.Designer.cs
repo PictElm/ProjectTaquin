8f596c9ec982be631f9f065b386242d27bc7814e
AdeBarros <45589639+AdeBarros@users.noreply.github.com>
2019-10-15 10:49:53 +0200

    Interface
    Add Game + Solver interface

---
+++ Taquin/App/Game.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace App
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TLP1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.TLP1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TLP1
+            // 
+            this.TLP1.ColumnCount = 3;
+            this.TLP1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP1.Controls.Add(this.btn1, 0, 0);
+            this.TLP1.Controls.Add(this.btn2, 1, 0);
+            this.TLP1.Controls.Add(this.btn3, 2, 0);
+            this.TLP1.Controls.Add(this.btn4, 0, 1);
+            this.TLP1.Controls.Add(this.btn5, 1, 1);
+            this.TLP1.Controls.Add(this.btn6, 2, 1);
+            this.TLP1.Controls.Add(this.btn7, 0, 2);
+            this.TLP1.Controls.Add(this.btn8, 1, 2);
+            this.TLP1.Controls.Add(this.btn9, 2, 2);
+            this.TLP1.Location = new System.Drawing.Point(22, 12);
+            this.TLP1.Name = "TLP1";
+            this.TLP1.RowCount = 3;
+            this.TLP1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP1.Size = new System.Drawing.Size(360, 360);
+            this.TLP1.TabIndex = 0;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(3, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(113, 113);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(123, 3);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(113, 113);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(243, 3);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(114, 113);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(3, 123);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(113, 113);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(123, 123);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(113, 113);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(243, 123);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(114, 113);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(3, 243);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(113, 114);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(123, 243);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(113, 114);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(243, 243);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(114, 114);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 381);
+            this.Controls.Add(this.TLP1);
+            this.Name = "GameForm";
+            this.Text = "Tak1Game";
+            this.TLP1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TLP1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+    }
+}
+
---
+++ Taquin/App/Solver.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace App
+{
+    partial class SolverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TLP1 = new System.Windows.Forms.TableLayoutPanel();
+            this.TLP2 = new System.Windows.Forms.TableLayoutPanel();
+            this.TV1 = new System.Windows.Forms.TreeView();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.TLP1.SuspendLayout();
+            this.TLP2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TLP1
+            // 
+            this.TLP1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TLP1.ColumnCount = 2;
+            this.TLP1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 39.46541F));
+            this.TLP1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60.53459F));
+            this.TLP1.Controls.Add(this.TLP2, 1, 0);
+            this.TLP1.Controls.Add(this.TV1, 0, 0);
+            this.TLP1.Location = new System.Drawing.Point(24, 14);
+            this.TLP1.Name = "TLP1";
+            this.TLP1.RowCount = 1;
+            this.TLP1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TLP1.Size = new System.Drawing.Size(600, 381);
+            this.TLP1.TabIndex = 0;
+            // 
+            // TLP2
+            // 
+            this.TLP2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TLP2.ColumnCount = 3;
+            this.TLP2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP2.Controls.Add(this.btn1, 0, 0);
+            this.TLP2.Controls.Add(this.btn2, 1, 0);
+            this.TLP2.Controls.Add(this.btn3, 2, 0);
+            this.TLP2.Controls.Add(this.btn4, 0, 1);
+            this.TLP2.Controls.Add(this.btn5, 1, 1);
+            this.TLP2.Controls.Add(this.btn6, 2, 1);
+            this.TLP2.Controls.Add(this.btn7, 0, 2);
+            this.TLP2.Controls.Add(this.btn8, 1, 2);
+            this.TLP2.Controls.Add(this.btn9, 2, 2);
+            this.TLP2.Location = new System.Drawing.Point(239, 3);
+            this.TLP2.Name = "TLP2";
+            this.TLP2.RowCount = 3;
+            this.TLP2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.TLP2.Size = new System.Drawing.Size(358, 375);
+            this.TLP2.TabIndex = 1;
+            // 
+            // TV1
+            // 
+            this.TV1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TV1.Location = new System.Drawing.Point(3, 3);
+            this.TV1.Name = "TV1";
+            this.TV1.Size = new System.Drawing.Size(230, 375);
+            this.TV1.TabIndex = 2;
+            // 
+            // btn1
+            // 
+            this.btn1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn1.Location = new System.Drawing.Point(3, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(113, 118);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn2.Location = new System.Drawing.Point(122, 3);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(113, 118);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn3.Location = new System.Drawing.Point(241, 3);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(114, 118);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn4.Location = new System.Drawing.Point(3, 127);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(113, 118);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn5.Location = new System.Drawing.Point(122, 127);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(113, 118);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn6.Location = new System.Drawing.Point(241, 127);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(114, 118);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn7.Location = new System.Drawing.Point(3, 251);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(113, 121);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn8.Location = new System.Drawing.Point(122, 251);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(113, 121);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn9.Location = new System.Drawing.Point(241, 251);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(114, 121);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // SolverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 414);
+            this.Controls.Add(this.TLP1);
+            this.Name = "SolverForm";
+            this.Text = "Tak1Solver";
+            this.TLP1.ResumeLayout(false);
+            this.TLP2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TLP1;
+        private System.Windows.Forms.TableLayoutPanel TLP2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.TreeView TV1;
+    }
+}
