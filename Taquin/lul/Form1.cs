8a8be47dfe1dd0ef8b092a7da4562372046f3780
AdeBarros <45589639+AdeBarros@users.noreply.github.com>
2019-10-15 10:20:35 +0200

    Interface

--- Taquin/lul/Form1.cs
+++ Taquin/lul/Form1.cs
@@ -8,13 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace lul
+namespace Interface
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
