97c2eb1683ae91e932a61a361f484b38d57dfb03
AdeBarros <45589639+AdeBarros@users.noreply.github.com>
2019-10-18 09:26:19 +0200

    Interface 0.1

--- Taquin/App/Solver.Designer.cs
+++ Taquin/App/Solver.Designer.cs
@@ -40,6 +40,8 @@
             this.btn8 = new System.Windows.Forms.Button();
             this.btn9 = new System.Windows.Forms.Button();
             this.lb1 = new System.Windows.Forms.ListBox();
+            this.btnLaunch = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
             this.TLP1.SuspendLayout();
             this.TLP2.SuspendLayout();
             this.SuspendLayout();
@@ -58,7 +60,7 @@
             this.TLP1.Name = "TLP1";
             this.TLP1.RowCount = 1;
             this.TLP1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.TLP1.Size = new System.Drawing.Size(600, 381);
+            this.TLP1.Size = new System.Drawing.Size(537, 345);
             this.TLP1.TabIndex = 0;
             // 
             // TLP2
@@ -79,13 +81,13 @@
             this.TLP2.Controls.Add(this.btn7, 0, 2);
             this.TLP2.Controls.Add(this.btn8, 1, 2);
             this.TLP2.Controls.Add(this.btn9, 2, 2);
-            this.TLP2.Location = new System.Drawing.Point(239, 3);
+            this.TLP2.Location = new System.Drawing.Point(214, 3);
             this.TLP2.Name = "TLP2";
             this.TLP2.RowCount = 3;
             this.TLP2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.TLP2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.TLP2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.TLP2.Size = new System.Drawing.Size(358, 375);
+            this.TLP2.Size = new System.Drawing.Size(320, 339);
             this.TLP2.TabIndex = 1;
             // 
             // btn1
@@ -95,112 +97,148 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btn1.Location = new System.Drawing.Point(3, 3);
             this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(113, 119);
+            this.btn1.Size = new System.Drawing.Size(100, 107);
             this.btn1.TabIndex = 0;
             this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
             // btn2
             // 
             this.btn2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn2.Location = new System.Drawing.Point(122, 3);
+            this.btn2.Location = new System.Drawing.Point(109, 3);
             this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(113, 119);
+            this.btn2.Size = new System.Drawing.Size(100, 107);
             this.btn2.TabIndex = 1;
             this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
             // 
             // btn3
             // 
             this.btn3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn3.Location = new System.Drawing.Point(241, 3);
+            this.btn3.Location = new System.Drawing.Point(215, 3);
             this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(114, 119);
+            this.btn3.Size = new System.Drawing.Size(102, 107);
             this.btn3.TabIndex = 2;
             this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
             // 
             // btn4
             // 
             this.btn4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn4.Location = new System.Drawing.Point(3, 128);
+            this.btn4.Location = new System.Drawing.Point(3, 116);
             this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(113, 119);
+            this.btn4.Size = new System.Drawing.Size(100, 107);
             this.btn4.TabIndex = 3;
             this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
             // 
             // btn5
             // 
             this.btn5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn5.Location = new System.Drawing.Point(122, 128);
+            this.btn5.Location = new System.Drawing.Point(109, 116);
             this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(113, 119);
+            this.btn5.Size = new System.Drawing.Size(100, 107);
             this.btn5.TabIndex = 4;
             this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
             // 
             // btn6
             // 
             this.btn6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn6.Location = new System.Drawing.Point(241, 128);
+            this.btn6.Location = new System.Drawing.Point(215, 116);
             this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(114, 119);
+            this.btn6.Size = new System.Drawing.Size(102, 107);
             this.btn6.TabIndex = 5;
             this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
             // 
             // btn7
             // 
             this.btn7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn7.Location = new System.Drawing.Point(3, 253);
+            this.btn7.Location = new System.Drawing.Point(3, 229);
             this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(113, 119);
+            this.btn7.Size = new System.Drawing.Size(100, 107);
             this.btn7.TabIndex = 6;
             this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
             // 
             // btn8
             // 
             this.btn8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn8.Location = new System.Drawing.Point(122, 253);
+            this.btn8.Location = new System.Drawing.Point(109, 229);
             this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(113, 119);
+            this.btn8.Size = new System.Drawing.Size(100, 107);
             this.btn8.TabIndex = 7;
             this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
             // 
             // btn9
             // 
             this.btn9.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn9.Location = new System.Drawing.Point(241, 253);
+            this.btn9.Location = new System.Drawing.Point(215, 229);
             this.btn9.Name = "btn9";
-            this.btn9.Size = new System.Drawing.Size(114, 119);
+            this.btn9.Size = new System.Drawing.Size(102, 107);
             this.btn9.TabIndex = 8;
             this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
             // 
             // lb1
             // 
+            this.lb1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lb1.FormattingEnabled = true;
             this.lb1.Location = new System.Drawing.Point(3, 3);
             this.lb1.Name = "lb1";
-            this.lb1.Size = new System.Drawing.Size(230, 368);
+            this.lb1.Size = new System.Drawing.Size(205, 329);
             this.lb1.TabIndex = 2;
             this.lb1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnLaunch
+            // 
+            this.btnLaunch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLaunch.Location = new System.Drawing.Point(272, 364);
+            this.btnLaunch.Name = "btnLaunch";
+            this.btnLaunch.Size = new System.Drawing.Size(96, 25);
+            this.btnLaunch.TabIndex = 1;
+            this.btnLaunch.Text = "Launch Solver";
+            this.btnLaunch.UseVisualStyleBackColor = true;
+            this.btnLaunch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReset.Location = new System.Drawing.Point(401, 364);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(77, 25);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // SolverForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(643, 414);
+            this.ClientSize = new System.Drawing.Size(580, 397);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnLaunch);
             this.Controls.Add(this.TLP1);
             this.Name = "SolverForm";
             this.Text = "Tak1Solver";
@@ -224,5 +262,7 @@
         private System.Windows.Forms.Button btn8;
         private System.Windows.Forms.Button btn9;
         private System.Windows.Forms.ListBox lb1;
+        private System.Windows.Forms.Button btnLaunch;
+        private System.Windows.Forms.Button btnReset;
     }
 }
--- Taquin/App/Solver.cs
+++ Taquin/App/Solver.cs
@@ -16,13 +16,23 @@ namespace App
         public SolverForm()
         {
             InitializeComponent();
+            BtnList = new List<Button> { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
+            foreach (Button button in BtnList)
+            {
+                button.Text = "0";
+                button.BackColor = Color.White;
+                button.ForeColor = Color.White;
+                currentBtnNb = 1;
+            }
         }
         
         public List<Button> BtnList;
+        public Solution.Step selectedNode;
+        public int currentBtnNb = 1;
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            selectedNode = (int[,])lb1.SelectedItem;
+            selectedNode = (Solution.Step) lb1.SelectedItem;
             BtnList = new List<Button> { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
             int m = 0;
             int n = 0;
@@ -33,7 +43,7 @@ namespace App
                     m = 0;
                     n++;
                 }
-                BtnList[i].Text = selectedNode[n, m].ToString();
+                BtnList[i].Text = selectedNode.grid[n, m].ToString();
                 if (BtnList[i].Text == "0")
                 {
                     BtnList[i].BackColor = Color.White;
@@ -46,6 +56,162 @@ namespace App
                 }
                 m++;
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,] currentGrid = { { int.Parse(btn1.Text), int.Parse(btn2.Text), int.Parse(btn3.Text) }, 
+                { int.Parse(btn4.Text), int.Parse(btn5.Text), int.Parse(btn6.Text) }, 
+                { int.Parse(btn7.Text), int.Parse(btn8.Text), int.Parse(btn9.Text) } };
+
+            Game newGame = new Game(3, countGaps(currentGrid));
+            Solve3 solver = new Solve3();
+            Solution solution = solver.Solve(newGame, getFinalGrid(3, countGaps(currentGrid)));
+            lb1.DataSource = solution.Steps;
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (btn1.Text == "" || btn1.Text == "0")
+            {
+                btn1.Text = currentBtnNb.ToString();
+                btn1.BackColor = Color.DarkGray;
+                btn1.ForeColor = Color.White;
+                currentBtnNb++;
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (btn2.Text == "" || btn2.Text == "0")
+            {
+                btn2.Text = currentBtnNb.ToString();
+                btn2.BackColor = Color.DarkGray;
+                btn2.ForeColor = Color.White;
+                currentBtnNb++;
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (btn3.Text == "" || btn3.Text == "0")
+            {
+                btn3.Text = currentBtnNb.ToString();
+                btn3.BackColor = Color.DarkGray;
+                btn3.ForeColor = Color.White;
+                currentBtnNb++;
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (btn4.Text == "" || btn4.Text == "0")
+            {
+                btn4.Text = currentBtnNb.ToString();
+                btn4.BackColor = Color.DarkGray;
+                btn4.ForeColor = Color.White;
+                currentBtnNb++;
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (btn5.Text == "" || btn5.Text == "0")
+            {
+                btn5.Text = currentBtnNb.ToString();
+                btn5.BackColor = Color.DarkGray;
+                btn5.ForeColor = Color.White;
+                currentBtnNb++;
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (btn6.Text == "" || btn6.Text == "0")
+            {
+                btn6.Text = currentBtnNb.ToString();
+                btn6.BackColor = Color.DarkGray;
+                btn6.ForeColor = Color.White;
+                currentBtnNb++;
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (btn7.Text == "" || btn7.Text == "0")
+            {
+                btn7.Text = currentBtnNb.ToString();
+                btn7.BackColor = Color.DarkGray;
+                btn7.ForeColor = Color.White;
+                currentBtnNb++;
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (btn8.Text == "" || btn8.Text == "0")
+            {
+                btn8.Text = currentBtnNb.ToString();
+                btn8.BackColor = Color.DarkGray;
+                btn8.ForeColor = Color.White;
+                currentBtnNb++;
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (btn9.Text == "" || btn9.Text == "0")
+            {
+                btn9.Text = currentBtnNb.ToString();
+                btn9.BackColor = Color.DarkGray;
+                btn9.ForeColor = Color.White;
+                currentBtnNb++;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            BtnList = new List<Button> { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
+            foreach (Button button in BtnList)
+            {
+                button.Text = "0";
+                button.BackColor = Color.White;
+                button.ForeColor = Color.White;
+                currentBtnNb = 1;
+            }
+        }
+
+        private int countGaps(int[,] grid)
+        {
+            int nbGaps = 0;
+            foreach (int elem in grid)
+            {
+                if (elem == 0)
+                {
+                    nbGaps++;
+                }
+            }
+            return nbGaps;
+        }
+
+        private int[,] getFinalGrid(int size, int gaps)
+        {
+            int[,] template = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            int numbers = (int)Math.Pow(size, 2) - gaps;
+            int row = 0;
+            int col = 0;
+            for (int i = 0; i < numbers; i++)
+            {
+                template[row, col] = i + 1;
+                col++;
+                if (col > 2)
+                {
+                    row++;
+                    col = 0;
+                }
+            }
+            return template;
         }
     }
 }
